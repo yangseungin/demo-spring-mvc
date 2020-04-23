5a817c6e567142fdc31883eff4fffb0b2630a188
seungin yang <rhfpdk92@naver.com>
2020-04-24 00:12:59 +0900

    Consumable Media Types and Producible Media Types

--- src/main/java/com/giantdwarf/demospringmvc/SampleController.java
+++ src/main/java/com/giantdwarf/demospringmvc/SampleController.java
@@ -1,17 +1,21 @@
 package com.giantdwarf.demospringmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/hello")
 public class SampleController {
 
     //@PostMapping @PutMapping @PatchMapping //하나만 쓸때
-    @RequestMapping("/yang")
+    @RequestMapping(
+            value = "/hello",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
     @ResponseBody
-    public String helloYang() {
-        return "hello yang";
+    public String hello() {
+        return "hello";
     }
 
 //    @RequestMapping("/**") //배열로도 가능
--- src/test/java/com/giantdwarf/demospringmvc/SampleControllerTest.java
+++ src/test/java/com/giantdwarf/demospringmvc/SampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,17 +22,15 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(get("/hello/yang"))
+        mockMvc.perform(get("/hello")
+                    .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string("hello yang"))
-                .andExpect(handler().handlerType(SampleController.class))
-                .andExpect(handler().methodName("helloYang"))
         ;
-        mockMvc.perform(get("/hello/yang.json"))
-                .andDo(print())
-                .andExpect(status().isNotFound())
-        ;
+//        mockMvc.perform(get("/hello/yang.json"))
+//                .andDo(print())
+//                .andExpect(status().isNotFound())
+//        ;
 
 //        mockMvc.perform(get("/hi"))
 //                .andDo(print())
